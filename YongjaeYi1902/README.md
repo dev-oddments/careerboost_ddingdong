4e231ff25c3de382eafe1489af022fac4a811e1c
이용재 <dididy@knou.ac.kr>
2019-09-20 22:49:23 +0900

    Init: react-native

---
+++ YongjaeYi1902/README.md
@@ -0,0 +1,2 @@
+# careerboost_ddingdong
+> 모바일 | Android/iOS: 계산기 어플리케이션 
---
+++ YongjaeYi1902/rnCalculator/src/App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>boostcourse-ace</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
